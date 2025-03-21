14b70ee160b872d5e8145fc173c7fe9c21b01636
Omar Ismail <oismail91@gmail.com>
2025-03-21 07:21:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-output "foo_dir" {
-  value = "hello from root.aa"
+resource "random_string" "example" {
+  length  = 18
+  special = false
+  upper   = false
+}
+
+output "generated_random_string" {
+  value = random_string.example.result
 }
